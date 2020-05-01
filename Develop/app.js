2b5a3bf018cd544ce95474c422a2ed4a059bcc1f
Meianna Oeser <meianna.oeser@gmail.com>
2020-05-01 12:43:27 -0700

    added timeblocks and created for loop

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,21 @@
 var currentDate = moment().format("dddd" + ", " + "LL");
+var timeBlocks = $("#timeBlocks");
 
-// This displays the date in the header
+// This function displays current date in the header
 function displayDate() {
   $("#currentDay").prepend(currentDate);
 }
 displayDate();
+
+$("button").on("click", function () {
+  var dataId = $(this).attr("id");
+  var id = dataId.split("-")[0];
+  var userData = $("#" + id + "-input").val();
+  console.log(dataId, userData, "#" + id + "-input");
+  localStorage.setItem(id, userData);
+});
+
+// This loop makes it so that the event descriptions stay on screen when  we refresh
+for (i = 9; i <= 17; i++) {
+  $(`#${i}-input`).val(localStorage.getItem(i));
+}
