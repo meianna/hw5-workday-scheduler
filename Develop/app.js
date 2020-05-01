f660f42bd9df07fba2f13fe61129b5ccc2a84cf1
Meianna Oeser <meianna.oeser@gmail.com>
2020-05-01 16:38:05 -0700

    added comments to app.js and organized code

--- Develop/app.js
+++ Develop/app.js
@@ -1,11 +1,12 @@
 var currentDate = moment().format("dddd" + ", " + "LL");
-
 // This function displays current date in the header
 function displayDate() {
   $("#currentDay").prepend(currentDate);
 }
 displayDate();
 
+// This function identifies which time block button we click
+// and saves that text input to local storage.
 $("button").on("click", function () {
   var dataId = $(this).attr("id");
   var id = dataId.split("-")[0];
@@ -15,8 +16,8 @@ $("button").on("click", function () {
 });
 
 var currentHour = moment().hours();
-
-// This loop makes it so that the event descriptions stay on screen when  we refresh
+// This loop makes it so that the text stays on screen when
+// we refresh and color codes time blocks based on current hour.
 for (i = 9; i <= 17; i++) {
   $(`#${i}-input`).val(localStorage.getItem(i));
   if (i < currentHour) {
